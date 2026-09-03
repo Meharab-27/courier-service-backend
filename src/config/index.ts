57ac2d86e584bf8ed38f456c8d6d534377288486
Meharab-27 <meharabrahman100115@gmail.com>
2026-09-04 00:31:15 +0600

    start implementing redis

--- src/config/index.ts
+++ src/config/index.ts
@@ -21,6 +21,6 @@ export default {
     redis_user:process.env.REDIS_USER!,
 	redis_password:process.env.REDIS_PASSWORD!,
 	redis_host:process.env.REDIS_HOST!,
-	redis_port:process.env.REDIS_PORT!,
+	redis_port:process.env.REDIS_PORT!
 
 }
